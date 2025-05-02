efed27b069534f6ee80ef1c5da3154b4c86b88e0
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-05-02 20:46:15 -0300

    corregi error en metodo mostrarResultado

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -7,48 +7,46 @@ Por último, utilice el método imprime datos para mostrar por pantalla los valo
 de los tres objetos instanciados.
 */
 
-class Producto{
-    #codigo
-    #nombre
-    #precio
+class Producto {
+  #codigo;
+  #nombre;
+  #precio;
 
-    
-    constructor(codigo, nombre, precio){
-        this.#codigo = codigo;
-        this.#nombre = nombre;
-        this.#precio = precio;
-    }
+  constructor(codigo, nombre, precio) {
+    this.#codigo = codigo;
+    this.#nombre = nombre;
+    this.#precio = precio;
+  }
 
-    get getCodigo(){
-        return this.#codigo;
-    }
+  get getCodigo() {
+    return this.#codigo;
+  }
 
-    set setCodigo(nuevoCodigo){
-        this.#codigo = nuevoCodigo;
-    }
+  set setCodigo(nuevoCodigo) {
+    this.#codigo = nuevoCodigo;
+  }
 
-    get getNombre(){
-        return this.#nombre;
-    }
+  get getNombre() {
+    return this.#nombre;
+  }
 
-    set setNombre(nuevoNombre){
-        this.#nombre = nuevoNombre;
-    }
+  set setNombre(nuevoNombre) {
+    this.#nombre = nuevoNombre;
+  }
 
-    get getPrecio(){
-        return this.#precio;
-    }
+  get getPrecio() {
+    return this.#precio;
+  }
 
-    set setPrecio(nuevoPrecio){
-        this.#precio = nuevoPrecio;
-    }
+  set setPrecio(nuevoPrecio) {
+    this.#precio = nuevoPrecio;
+  }
 
-    mostrarResultado() {
-        document.writeln(`<ul>:
-            <li>Codigo: ${this.getCodigo()}</li>
-             <li>Nombre: ${this.getNombre()}</li>
-             <li>Precio: ${this.getPrecio()}</li>
-             </ul>`
-        );
-      }
+  mostrarResultado() {
+    document.writeln(`<ul>:
+            <li>Codigo: ${this.getCodigo}</li>
+             <li>Nombre: ${this.getNombre}</li>
+             <li>Precio: ${this.getPrecio}</li>
+             </ul>`);
+  }
 }
